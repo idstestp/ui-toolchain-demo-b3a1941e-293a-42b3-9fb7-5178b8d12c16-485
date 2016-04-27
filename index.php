93643fd4110627556895ce343a64d6e4064e526c
bluemix_ui_load0323t001@mailinator.com <bluemix_ui_load0323t001@mailinator.com>
2016-04-27 13:08:20 +0000

    JMeter Test Updates: 2016-04-27 03:08

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ function orderItem(itemID){
 </head>
 <table class="headerTable">
 	<tr>
-		<td><span class="pageTitle"><h1>Microservices Sample : Updated by JMeter: 824</h1></span></td> 
+		<td><span class="pageTitle"><h1>Microservices Sample : Updated by JMeter: 952</h1></span></td> 
 	</tr>
 </table>
 <body onload="loadItems(items)">
